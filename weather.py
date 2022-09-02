5d7e44f9a909acf8477b5c99e9f0bc37ace2d8f6
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-09-02 14:05:47 +0200

    Added weather API

--- weather.py
+++ weather.py
@@ -4,6 +4,13 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "ebeebe_ebeebe"
 
+API_KEY = "15959927002cae2d94c39c62f2266558"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = str(request.form['name_input'])
+request_url = f"{BASE_URL}?appid={API_KEY}&lang=en&q={city}"
+response = requests.get(request_url)
+
 @app.route("/weather")
 def index():
     flash("Enter a city name:")
@@ -11,23 +18,12 @@ def index():
 
 @app.route("/outcome", methods=["POST", "GET"])
 def weather():
-    flash("Weather in " + str(request.form['name_input']) + "is: ")
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['weather'][0]['description'].title()
+        temperature = str(round(data['main']['temp'] - 273.15)) + " °C"
+        flash("Weather in " + city + " is: " + weather + " " + temperature)
+    else:
+        error = "Error: " + str(response.status_code)
+        flash(error)
     return render_template("index.html")
-    
-"""
-API_KEY = "15959927002cae2d94c39c62f2266558"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
-
-city = input("Enter city name: ")
-request_url = f"{BASE_URL}?appid={API_KEY}&lang=en&q={city}"
-response = requests.get(request_url)
-
-if response.status_code == 200:
-    data = response.json()
-    weather = data['weather'][0]['description'].title()
-    print(weather)
-    temperature = str(round(data['main']['temp'] - 273.15)) + " °C"
-    print(temperature)
-else:
-    print(f"{response.status_code}")
-"""
