0d60778b8935a1a9570ad282efdb86588be2b42d
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-09-02 13:30:28 +0200

    Introduced flask

--- weather.py
+++ weather.py
@@ -1,10 +1,25 @@
 import requests
+from flask import Flask, render_template, request, flash
 
+app = Flask(__name__)
+app.secret_key = "ebeebe_ebeebe"
+
+@app.route("/weather")
+def index():
+    flash("Enter a city name:")
+    return render_template("index.html")
+
+@app.route("/outcome", methods=["POST", "GET"])
+def weather():
+    flash("Weather in " + str(request.form['name_input']) + "is: ")
+    return render_template("index.html")
+    
+"""
 API_KEY = "15959927002cae2d94c39c62f2266558"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter city name: ")
-request_url = f"{BASE_URL}?appid={API_KEY}&lang=pl&q={city}"
+request_url = f"{BASE_URL}?appid={API_KEY}&lang=en&q={city}"
 response = requests.get(request_url)
 
 if response.status_code == 200:
@@ -15,3 +30,4 @@ if response.status_code == 200:
     print(temperature)
 else:
     print(f"{response.status_code}")
+"""
