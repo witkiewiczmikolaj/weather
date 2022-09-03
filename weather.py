8829b67ba0bb45d90bf3c46532269481b51e3937
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-09-03 11:36:27 +0200

    Changed API and added days to choose from

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.secret_key = "ebeebe_ebeebe"
 
 API_KEY = "15959927002cae2d94c39c62f2266558"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+BASE_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
 @app.route("/weather")
 def index():
@@ -14,16 +14,22 @@ def index():
 
 @app.route("/outcome", methods=["POST", "GET"])
 def weather():
+    
     city = str(request.form['name_input']).title()
     request_url = f"{BASE_URL}?appid={API_KEY}&lang=en&q={city}"
     response = requests.get(request_url)
-
+    data = response.json() 
+        
     if response.status_code == 200:
-        data = response.json()
-        weather = data['weather'][0]['description'].title()
-        temperature = str(round(data['main']['temp'] - 273.15)) + " °C"
-        flash("Weather in " + city + " is: " + weather + " " + temperature)
+            
+        sliderValue = request.form['slider_value']
+        day = int(sliderValue) * 8
+        weather = data['list'][day]['weather'][0]['description'].title()
+        temperature = str(round(data['list'][day]['main']['temp'] - 273.15)) + " °C"
+        flash("[Day " + sliderValue + "] weather in " + city + " is: " + weather + " " + temperature)
+
     else:
         error = "I couldn't find that city :( Error: " + str(response.status_code)
         flash(error)
     return render_template("index.html")
+    
