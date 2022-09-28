0b2ae30328713d98436f6ba49ac16c418eeb7c13
witkiewiczmikolaj <witkiewiczmikolaj@gmail.com>
2022-09-28 15:32:25 +0200

    Plot adjustments

--- weather.py
+++ weather.py
@@ -1,9 +1,10 @@
 import requests
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
 from io import BytesIO
 from matplotlib.figure import Figure
 import base64
 import numpy as np
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.secret_key = "xxxxxxxxxxxxxxxxxxx"
@@ -32,20 +33,29 @@ def weather():
         temperature = str(round(data['list'][day]['main']['temp'] - 273.15)) + " °C"
         resp = ("[Day " + sliderValue + "] weather in " + city + " is: " + weather + " " + temperature)
 
-        fig = Figure()
+        fig = Figure(figsize=(18, 6), dpi=70, facecolor='#292929')
         y = []
         x = []
-        x = 0.5 + np.arange(8)
         for i in range (0,8):
             y.append(round(data['list'][day + i]['main']['temp'] - 273.15))
-        
+            now = datetime.now() + timedelta(hours = i * 3)
+            current_time = now.strftime("%H:%M")
+            x.append(current_time)
+            
         max_temp = max(y) + 2
         
         ax = fig.subplots()
-        ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
-
-        ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
-               ylim=(0, max_temp), yticks=np.arange(0, max_temp, 2))
+        ax.set_facecolor("#292929")
+        ax.set_xlabel('Hour', color="#F15738")
+        ax.set_ylabel('°C', rotation=0, loc="top", color="#F15738")
+        ax.bar(x, y, width=0.7, color="#FBAF40", edgecolor="#F15738", linewidth=3)
+        ax.spines['bottom'].set_color('#292929')
+        ax.spines['top'].set_color('#292929') 
+        ax.spines['right'].set_color('#292929')
+        ax.spines['left'].set_color('#292929')
+        ax.tick_params(axis='x', colors='#F15738')
+        ax.tick_params(axis='y', colors='#F15738')
+        ax.set(xlim=(-0.5, 7.5), xticks=np.arange(0, 8), ylim=(0, max_temp), yticks=np.arange(0, max_temp, 2))
 
         buf = BytesIO()
         fig.savefig(buf, format="png")
@@ -54,8 +64,8 @@ def weather():
         img = f"<img src='data:image/png;base64,{data}'/>"
 
     else:
-        resp = "I couldn't find that city :( Error: " + str(response.status_code)
-        
+        resp = "I couldn't find that city :("
+        img = "<p>Error:" + str(response.status_code) + "</p>"
     return render_template("outcome.html", respp = resp, img = img)
 
 if __name__ == "__main__":
